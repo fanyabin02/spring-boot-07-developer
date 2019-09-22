a9b1e413889c3d30586e8abcdf7948f519796b42
root_global <root_global@atguigu.com>
2019-09-22 13:38:24 +0800

    分支合并二次
    # Conflicts:
    #	src/main/java/com/atguigu/developer/controller/HelloController.java

--- src/main/java/com/atguigu/developer/controller/HelloController.java
+++ src/main/java/com/atguigu/developer/controller/HelloController.java
@@ -12,8 +12,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        System.out.println("测试====分支");
-        System.out.println("二次测试分支");
         System.out.println("second commit");
         return "abc";
     }
