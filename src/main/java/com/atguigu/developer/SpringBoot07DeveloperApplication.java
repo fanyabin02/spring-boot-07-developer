046f7f8045cf260333de284182461048c7004138
root_global <root_global@atguigu.com>
2019-09-22 11:38:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/developer/SpringBoot07DeveloperApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 1.导入starter
+ * 2.ctrl+f9刷新
+ */
+@SpringBootApplication
+public class SpringBoot07DeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot07DeveloperApplication.class, args);
+	}
+}
---
+++ src/main/java/com/atguigu/developer/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.atguigu.developer.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("aa");
+        return "abc";
+    }
+
+
+}
